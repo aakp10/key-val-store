3e339619cebc3377b919cd3171f93773bbb92e5c
Karuna Grewal <karunagrewal98@gmail.com>
2019-09-20 02:01:48 +0530

    extend impl of key value store

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
 use std::path::{Path};
 use std::io::prelude::*;
-use std::io::{BufReader, SeekFrom};
+use std::io::{BufReader, SeekFrom, BufWriter};
 use std::fs::{File, OpenOptions};
 use std::io;
-use byteorder::{LittleEndian, ReadBytesExt};
-use crc::crc32;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use crc::{crc32};
 
 type ByteString = Vec<u8>;
 type ByteStr = [u8];
+
 pub struct KeyValuePair {
     key: ByteString,
     value: ByteString,
@@ -34,14 +35,87 @@ impl KVStore {
         Ok(store_instance)
     }
 
-    pub fn load(&mut self) {
+    pub fn load(&mut self) -> io::Result<()> {
         let mut file_buffer = BufReader::new(&mut self.file);
         loop {
             // TODO: https://stackoverflow.com/questions/34878970/how-to-get-current-cursor-position-in-file/
-            let current_pos = file_buffer.seek(SeekFrom::Current(0));
+            let current_pos = file_buffer.seek(SeekFrom::Current(0))?;
             //get the key value pair stored at this location
             let current_kv_pair = process_record(&mut file_buffer);
+            let kv = match current_kv_pair {
+                Ok(kv) => kv,
+                Err(err) => {
+                    match err.kind() {
+                        io::ErrorKind::UnexpectedEof => {
+                            break;
+                        },
+                        _ => return Err(err),
+                    }
+                },
+            };
+ 
+            self.index.insert(kv.key, current_pos);
         }
+        Ok(())
+    }
+
+    fn seek_to_end(&mut self) {
+        self.file.seek(SeekFrom::End(0)).expect("Error changing the current position by the sepicified offset");
+    }
+
+    fn get_to_position(&mut self, position: u64) {
+        self.file.seek(SeekFrom::Start(position));
+    }
+
+    pub fn insert(&mut self, key: &ByteStr, val: &ByteStr) -> io::Result<()> {
+        //bufReader of the file
+        self.seek_to_end();
+        let mut file_buffer = BufWriter::new(&mut self.file);
+
+        //convert to the req storage format
+        let key_len = key.len();
+        let val_len = val.len();
+        let data_len = (key_len + val_len) as usize;
+        let mut data_buffer = ByteString::with_capacity(data_len);
+        //insert key and value into this data_buffer
+        for &byte in key {
+            data_buffer.push(byte);
+        }
+        for &byte in val {
+            data_buffer.push(byte);
+        }
+        //calculate checksum
+        let checksum = crc32::checksum_ieee(&data_buffer);
+        let cur_position = file_buffer.seek(SeekFrom::Current(0)).expect("Error changing the current position by the sepicified offset");
+        file_buffer.write_u32::<LittleEndian>(checksum).expect("Error writing to buffer");
+        file_buffer.write_u32::<LittleEndian>(key_len as u32).expect("Error writing to buffer");
+        file_buffer.write_u32::<LittleEndian>(val_len as u32).expect("Error writing to buffer");
+        file_buffer.write_all(&data_buffer[..]).expect("Error writing to buffer");
+        
+        self.index.insert(key.to_vec(), cur_position);
+        Ok(())
+    }
+
+    pub fn update(&mut self, key: &ByteStr, val: &ByteStr) -> io::Result<()> {
+        self.insert(key, val)
+    }
+
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
+    }
+
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        //fetch the position from the kv hashmap
+        let position = match self.index.get(key) {
+            None => return Ok(None),
+            Some(position) => *position,
+        };
+        //seek the file ref to `position`
+        self.get_to_position(position);
+        //process the corresonding record
+        let mut file_buffer = BufReader::new(&mut self.file);
+        let kv_pair = process_record(&mut file_buffer)?;
+        Ok(Some(ByteString::from(kv_pair.value)))
     }
 
 }
@@ -71,4 +145,4 @@ pub fn process_record<R: Read>(file_buffer: &mut R) -> io::Result<KeyValuePair>{
         let key = buffer;
         //return this {key,value} wrapped in a Result instance
         Ok(KeyValuePair{key, value})
-    }
+}
